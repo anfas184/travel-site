8b7a64d6935763dcc2744a593176b523bf9591ac
Anfas <mohamedanfas184@gmail.com>
2018-05-28 02:37:05 +0530

    completed desktop and mobile header styles

---
+++ app/assets/styles/scripts/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+	$('.site-header__menu-icon').click(function () {
+		$(".site-header").toggleClass("site-header--is-expanded");
+		$(".site-header__menu-content").toggleClass("site-header__menu-content--is-visible");
+		$(".site-header__menu-icon").toggleClass("site-header__menu-icon--close-x");
+
+	});
+
+});
