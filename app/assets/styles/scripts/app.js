b37a8cc5cc67a78fd74cd083e25b55319f9323b3
Anfas <mohamedanfas184@gmail.com>
2018-05-28 18:00:19 +0530

    completed styles for sticky navigation

--- app/assets/styles/scripts/app.js
+++ app/assets/styles/scripts/app.js
@@ -4,13 +4,24 @@ $(document).ready(function () {
 		$(".site-header").toggleClass("site-header--is-expanded");
 		$(".site-header__menu-content").toggleClass("site-header__menu-content--is-visible");
 		$(".site-header__menu-icon").toggleClass("site-header__menu-icon--close-x");
-
 	});
-	
 	//reveal header on scroll
+	
+	   /*  for the sticky navigation  */
+    $('.large-hero__title').waypoint(function (direction) {
 
+        if (direction == "down") {
+            $('.site-header').addClass('site-header--dark');
+        } else {
+            $('.site-header').removeClass('site-header--dark');
+        }
 
+    }, {
+        offset: '40px;'
+    });
+	
 });
+
 
 
 
